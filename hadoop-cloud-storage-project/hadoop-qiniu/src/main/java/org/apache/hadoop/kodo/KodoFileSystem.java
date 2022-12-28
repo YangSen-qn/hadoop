71746fbb1d9ebf148b7ecc2bb12615cd287a67c9
zzq <i@zhangzqs.cn>
2022-12-28 13:05:17 +0800

    optimize arguments name

--- hadoop-cloud-storage-project/hadoop-qiniu/src/main/java/org/apache/hadoop/kodo/KodoFileSystem.java
+++ hadoop-cloud-storage-project/hadoop-qiniu/src/main/java/org/apache/hadoop/kodo/KodoFileSystem.java
@@ -10,51 +10,46 @@ import java.io.IOException;
 import java.net.URI;
 
 public class KodoFileSystem extends FileSystem {
-
-
     @Override
-    public FSDataInputStream open(Path path, int i) throws IOException {
+    public FSDataInputStream open(Path f, int bufferSize) throws IOException {
         return null;
     }
 
     @Override
-    public FSDataOutputStream create(Path path, FsPermission fsPermission, boolean b, int i, short i1, long l, Progressable progressable) throws IOException {
+    public FSDataOutputStream create(Path f, FsPermission permission, boolean overwrite, int bufferSize, short replication, long blockSize, Progressable progress) throws IOException {
         return null;
     }
 
-
-
     @Override
-    public boolean rename(Path path, Path path1) throws IOException {
+    public boolean rename(Path src, Path dst) throws IOException {
         return false;
     }
 
     @Override
-    public boolean delete(Path path, boolean b) throws IOException {
+    public boolean delete(Path f, boolean recursive) throws IOException {
         return false;
     }
 
     @Override
-    public FileStatus[] listStatus(Path path) throws FileNotFoundException, IOException {
+    public FileStatus[] listStatus(Path f) throws FileNotFoundException, IOException {
         return new FileStatus[0];
     }
 
-
-
-
     @Override
-    public boolean mkdirs(Path path, FsPermission fsPermission) throws IOException {
-        return true;
+    public boolean mkdirs(Path f, FsPermission permission) throws IOException {
+        return false;
     }
 
     @Override
-    public FileStatus getFileStatus(Path path) throws IOException {
+    public FileStatus getFileStatus(Path f) throws IOException {
         return null;
     }
+
 
     private String bucket;
     private Path workingDir;
     private URI uri;
+
     @Override
     public void initialize(URI name, Configuration conf) throws IOException {
         super.initialize(name, conf);
@@ -70,19 +65,19 @@ public class KodoFileSystem extends FileSystem {
     }
 
     @Override
-    public FSDataOutputStream append(Path path, int i, Progressable progressable) throws IOException {
+    public FSDataOutputStream append(Path f, int bufferSize, Progressable progress) throws IOException {
         throw new IOException("Not supported");
     }
+
     @Override
-    public void setWorkingDirectory(Path path) {
-        this.workingDir = path;
+    public void setWorkingDirectory(Path f) {
+        this.workingDir = f;
     }
 
     @Override
     public Path getWorkingDirectory() {
         return this.workingDir;
     }
-
 
     @Override
     public String getScheme() {
